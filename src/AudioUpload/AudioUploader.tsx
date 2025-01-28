1273a7ed7f7074c4a67d6a796225cea37c6d82cd
pixelynx <tjstevens1992@gmail.com>
2025-01-27 23:15:24 -0500

    add components for = player and audio+transcript upload

---
+++ src/AudioUpload/AudioUploader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const AudioUploader = () => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default AudioUploader;
---
+++ src/TranscriptUpload/TranscriptUploader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TranscriptUploader = () => {
+
+  return (
+    <form>
+      
+    </form>
+  );
+};
+
+export default TranscriptUploader;
